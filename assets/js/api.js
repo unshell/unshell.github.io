39b70a4c1341cc7ad7a07b450106a26eb245f54e
五岁能抬头 <1548462908@qq.com>
2024-10-08 10:54:43 +0800

    主题切换
    黑白主题切换

--- assets/js/api.js
+++ assets/js/api.js
@@ -44,6 +44,27 @@ function params() {
 
 /* Request loading */
 function loading(end) {
-    var $target = $('p.loading'), html = $target.html();
+    var $target = $('p.loading');
     $target.css('display', end ? 'none' : 'inherit');
 }
+
+/* Theme ready */
+function setTheme() {
+    var theme = localStorage.getItem('theme');
+    if (!theme && window.matchMedia) { // system theme color
+        theme = window.matchMedia("(prefers-color-scheme: dark)").matches ? 'black' : 'white';
+        localStorage.setItem('theme', theme);
+    }
+    var $icon = $('svg[data-theme="' + theme + '"]');
+    $('body').removeClass().addClass(theme);
+    $('svg.icon').removeClass('active');
+    $icon.addClass('active');
+}
+
+/* Document click */
+$(document).on('click', 'svg.icon', function () {
+    var $this = $(this), $sibling = $this.siblings('svg.icon'), data = $sibling.data(), theme = data.theme;
+    $this.removeClass('active').siblings('svg.icon').addClass('active');
+    localStorage.setItem('theme', theme);
+    setTheme();
+});
