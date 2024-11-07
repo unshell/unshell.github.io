2dc6f570d401199d83a093399a04287d086321f4
五岁能抬头 <1548462908@qq.com>
2024-11-07 16:01:51 +0800

    .
    .

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,11 +1,43 @@
 /**
+ * Blog specific token
+ */
+var owner = 'unshell', repo = 'posts', token = ['git', 'hub', '_p', 'a', 't_', '11AHBQ7GY0', '9bxCAqoA0GeT', '_J04BTjBx2jF', 'jaPRgfdTlJiJ01', '98kNv2iFOzrctmcIV', '4Z5VHDL6ZhyV5UzbF'].join('');
+
+/**
+ * Update an issue
+ * 
+ * @param {*} success 成功的回调
+ * @param {*} body 修改的内容
+ */
+function update(success, body, number) {
+    $.ajax({
+        url: 'https://api.github.com/repos/' + owner + '/' + repo + '/issues/' + number,
+        type: 'PATCH',
+        headers: {
+            'Accept': 'application/vnd.github+json',
+            'Authorization': 'token ' + token,
+            'X-GitHub-Api-Version': '2022-11-28'
+        },
+        success: success,
+        data: JSON.stringify({
+            title: 'JSON DATEBASE',
+            body: JSON.stringify(body),
+            state: 'closed' // 始终处于关闭状态
+        }),
+        error: function (e) {
+            alert(e.responseJSON.message);
+        }
+    });
+}
+
+/**
  * List repository issues
  * 
  * @param {*} success 成功的回调
  */
 function issues(success) {
     $.ajax({
-        url: 'https://api.github.com/repos/unshell/posts/issues',
+        url: 'https://api.github.com/repos/' + owner + '/' + repo + '/issues',
         headers: {
             accept: 'application/vnd.github+json'
         },
@@ -23,7 +55,7 @@ function issues(success) {
  */
 function issue(success, number) {
     $.ajax({
-        url: 'https://api.github.com/repos/unshell/posts/issues/' + number,
+        url: 'https://api.github.com/repos/' + owner + '/' + repo + '/issues/' + number,
         headers: {
             accept: 'application/vnd.github+json'
         },
@@ -59,6 +91,33 @@ function setTheme() {
     $('body').removeClass().addClass(theme);
     $('i.iconfont').removeClass('active');
     $icon.addClass('active');
+}
+
+/**
+ * Validate Json
+ * 
+ * @param {*} json Json 字符
+ * @param {*} warn 是否提醒
+ * @returns 
+ */
+function validateJson(json, warn) {
+    try {
+        json = parseJson(json);
+        return true;
+    } catch (e) {
+        var index = parseInt(e.message.match(/position (\d+)/)[1], 10);
+        warn && alert('JSON 格式错误，发生在以下语法附近：\n' + json.substring(index).replace(/\s+/g, ''));
+        return false;
+    }
+}
+
+/**
+ * Parse Json
+ * 
+ * @param {*} json 
+ */
+function parseJson(json) {
+    return typeof json == 'object' ? json : parseJson(JSON.parse(json));
 }
 
 /* Document click */
